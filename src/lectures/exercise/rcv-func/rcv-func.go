96e04fee713410108e95fefae9e3dcdcccff46a4
wolfpirker <w_pirker@gmx.de>
2022-06-12 01:56:14 +0200

    idiomatic go, notes and exercises - part 1/2

--- src/lectures/exercise/rcv-func/rcv-func.go
+++ src/lectures/exercise/rcv-func/rcv-func.go
@@ -16,6 +16,47 @@ package main
 
 import "fmt"
 
-func main() {
+type Player struct {
+	name string
+	health, maxHealth uint
+	energy, maxEnergy uint
 
+}
+
+//* Implement receiver functions to modify the `Health` and `Energy`
+//  statistics of the player.
+func (player *Player) changeHealthBy (healthDelta int){
+	if healthDelta > 0 {
+		player.health += uint(healthDelta)
+		if player.health > player.maxHealth{
+			player.health = player.maxHealth
+		} 	
+		fmt.Println("Health restored; add", healthDelta)
+	} else {
+		if player.health > uint(-healthDelta){
+			player.health -= uint(-healthDelta)
+		} else{
+			player.health = 0
+		}
+		fmt.Println("Health reduced by", healthDelta)
+	}	
+}
+
+func (player *Player) changeEnergyBy (healthDelta int){
+	// quite the same...
+}
+
+func main() {
+	hero := Player{
+		name: "Lukas",
+		health: 100,
+		maxHealth: 100,
+		energy: 100,
+		maxEnergy : 100,		
+	}
+
+	hero.changeHealthBy(-20)
+	hero.changeEnergyBy(-30)
+	hero.changeHealthBy(30)
+	hero.changeEnergyBy(10)
 }
