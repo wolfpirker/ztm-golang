dcc3ec122b52deb368f30c87aa53bec9f6655e94
wolfpirker <w_pirker@gmx.de>
2022-06-14 19:50:31 +0200

    interfaces in Go, part 1of3

--- src/lectures/exercise/errors/errors.go
+++ src/lectures/exercise/errors/errors.go
@@ -15,3 +15,59 @@
 //* Use the `errors` package to generate errors
 
 package timeparse
+
+import (
+	"strconv"
+	"strings"
+	"fmt"
+)
+
+type TimeParseError struct{
+	msg, input string
+}
+
+type  Time struct {
+	hour, minute, second int
+}
+
+//* If parsing fails, then a descriptive error must be returned
+func (t *TimeParseError) Error() string {
+	return fmt.Sprintf("%v: %v", t.msg, t.input)
+}
+
+//--Summary:
+//  Create a function that can parse time strings into component values.
+//* The function must parse a string into a struct containing:
+//  - Hour, minute, and second integer components
+func ParseTime(input string) (Time, error){
+	components := strings.Split(input, ":")
+	//* If parsing fails, then a descriptive error must be returned
+	if len(components) != 3 {
+		return Time{0, 0, 0}, &TimeParseError{"Invalid number of time components", input}
+	} else {
+		hour, err := strconv.Atoi(components[0])
+		if err != nil {
+			return Time{0, 0, 0}, &TimeParseError{fmt.Sprintf("Error parsing hour: %v", err), input}
+		}
+		minute, err := strconv.Atoi(components[1])
+		if err != nil {
+			return Time{0, 0, 0}, &TimeParseError{fmt.Sprintf("Error parsing minute: %v", err), input}
+		}
+		second, err := strconv.Atoi(components[2])
+		if err != nil {
+			return Time{0, 0, 0}, &TimeParseError{fmt.Sprintf("Error parsing second: %v", err), input}
+		}
+
+		if hour > 23 || hour < 0 {
+			return Time{0, 0, 0}, &TimeParseError{"hour out of range: 0 <= hour <= 23", fmt.Sprintf("%v", hour)}
+		}
+		if minute > 59 || minute < 0 {
+			return Time{0, 0, 0}, &TimeParseError{"minute out of range: 0 <= minute <= 59", fmt.Sprintf("%v", minute)}
+		}
+		if second > 59 || second < 0 {
+			return Time{0, 0, 0}, &TimeParseError{"second out of range: 0 <= second <= 59", fmt.Sprintf("%v", second)}
+		}
+
+		return Time{hour, minute, second}, nil
+	}
+}
