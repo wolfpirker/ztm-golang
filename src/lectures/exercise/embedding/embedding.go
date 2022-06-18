0d7e6bc0aeee19120cc41c3fb111c556b9d57d68
wolfpirker <w_pirker@gmx.de>
2022-06-18 21:52:32 +0200

    interfaces in Go, part 2of3; Reading, Writing & Type Embedding

--- src/lectures/exercise/embedding/embedding.go
+++ src/lectures/exercise/embedding/embedding.go
@@ -31,8 +31,67 @@ type MemoryUsage struct {
 	amount []Bytes
 }
 
+
+func (band *BandwidthUsage) GetAverageBandwidth() int {
+	var sum int
+	for item := range band.amount{
+		// Note: not put item! it just counts each iteration!
+		// sum+=item
+		sum+=int(band.amount[item])
+	}
+	if len(band.amount) > 0 {
+		return sum/len(band.amount)
+	}
+	return 0
+}
+
+func (temp *CpuTemp) GetAverageTemp() int {
+	var sum int
+	for item := range temp.temp{
+		//fmt.Println(temp.temp[item])
+		sum+=int(temp.temp[item])
+	}
+	if len(temp.temp) > 0 {
+		return sum/len(temp.temp)
+	}
+	return 0
+}
+
+func (mem *MemoryUsage) GetAverageMemory() int {
+	var sum int
+	for item := range mem.amount{
+		sum+=int(mem.amount[item])
+	}
+	if len(mem.amount) > 0 {
+		return sum/len(mem.amount)
+	}
+	return 0
+}
+
+//* Using struct embedding, create a Dashboard structure that contains
+//  each dashboard component
+type Dashboard struct{
+	BandwidthUsage
+	CpuTemp
+	MemoryUsage
+}
+
 func main() {
 	bandwidth := BandwidthUsage{[]Bytes{50000, 100000, 130000, 80000, 90000}}
 	temp := CpuTemp{[]Celcius{50, 51, 53, 51, 52}}
 	memory := MemoryUsage{[]Bytes{800000, 800000, 810000, 820000, 800000}}
+
+	dash := Dashboard{
+		BandwidthUsage: bandwidth,
+		CpuTemp:        temp,
+		MemoryUsage:    memory,
+	}
+
+	//* Print out a 5-second average from each component using promoted
+	//  methods on the Dashboard
+	fmt.Printf("Average bandwidth usage: %v\n", dash.GetAverageBandwidth())
+	fmt.Printf("Average temp: %v\n", dash.GetAverageTemp())
+	fmt.Printf("Average memory usage: %v\n", dash.GetAverageMemory())
+
+	// Note: not possible/or so easy if the methods all had the same name!
 }
--- src/lectures/exercise/reader/reader.go
+++ src/lectures/exercise/reader/reader.go
@@ -18,8 +18,60 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Statistics struct {
+	enteredCmnds int
+	linesOfTextEntered int
+}
+
+//* When the user enters either "hello" or "bye", the program
+//  should respond with a message after pressing the enter key.
+//* Whenever the user types a "Q" or "q", the program should exit.
+//* Upon program exit, some usage statistics should be printed
+//  ('Q' and 'q' do not count towards these statistics):
+//  - The number of non-blank lines entered
+//  - The number of commands entered
+func reactToCommand(cmd string, stat *Statistics) bool{
+	cmd = strings.ToUpper(cmd)
+	switch cmd {
+	case "HELLO":
+		stat.enteredCmnds+=1
+		stat.linesOfTextEntered+=1
+		fmt.Println("Hello there!")
+	case "BYE":
+		stat.enteredCmnds+=1
+		stat.linesOfTextEntered+=1
+		fmt.Println("Bye, have a nice day ;)")
+	case "Q":
+		fmt.Println("Entered commands ", stat.enteredCmnds)
+		fmt.Println("Entered lines ", stat.linesOfTextEntered)
+		return true
+	default:
+		fmt.Println("invalid command")
+		stat.linesOfTextEntered+=1
+	}
+	return false
+}
 
 func main() {
+	//  Create an interactive command line application that supports arbitrary
+	//  commands. When the user enters a command, the program will respond
+	//  with a message. The program should keep track of how many commands
+	//  have been ran, and how many lines of text was entered by the user.
 
+	var stats Statistics
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		if reactToCommand(scanner.Text(), &stats){
+			return
+		}
+	}
+	
 }
